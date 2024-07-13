a7b8a0b2871dfbe0a9db14f743255319485efffb
Fabricio Felix <felixfabricio115@gmail.com>
2024-07-13 15:03:47 -0300

    metodos read, buffers, unicode e encoding utf

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,43 @@
-﻿using ByteBankIO;
+﻿using System.Globalization;
+using System.Text;
+using ByteBankIO;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+        // Devoluções:
+        // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+        // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+        //zero, se o final do fluxo for atingido
+
+        // public override int Read(byte[] array, int offset, int count)
 
         Console.ReadLine();
+    }
+
+    private static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
     }
 }
