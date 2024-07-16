d6ddbc9c2c956fc71d1b254c1f6e1874fbc56e92
Fabricio Felix <felixfabricio115@gmail.com>
2024-07-16 09:59:32 -0300

    CLOSE AND USING

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,39 +6,39 @@ class Program
 {
     static void Main(string[] args)
     {
+
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
 
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-        var buffer = new byte[1024]; //1KB
-
-        while (numeroDeBytesLidos != 0)
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024]; //1KB      
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            // Devoluções:
+            //  O número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+            //  bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+            //  zero, se o final do fluxo for atingido
+
+
+            // public override int Read(byte[] array, int offset, int count);
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
         }
-
-        // Devoluções:
-        // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
-        // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
-        //zero, se o final do fluxo for atingido
-
-        // public override int Read(byte[] array, int offset, int count)
-
-        Console.ReadLine();
     }
 
     private static void EscreverBuffer(byte[] buffer)
     {
         var utf8 = new UTF8Encoding();
-        {
-            foreach (var meuByte in buffer)
-            {
-                Console.Write(meuByte);
-                Console.Write(" ");
-            }
-        }
+        var texto = utf8.GetString(buffer);
+        Console.WriteLine(texto);
     }
 }
 //aula 02
