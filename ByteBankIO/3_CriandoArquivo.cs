d4546c52477807540f7507fbbfc29a3841eae6a9
fabriciofelix <felixfabricio115@gmail.com>
2024-07-24 10:18:02 -0300

    inicio de 4 etapa

--- ByteBankIO/3_CriandoArquivo.cs
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -1,19 +1,28 @@
 using System.Text;
 
-namespace ByteBankIO;
-
-public class Program {
-
+partial class Program
+{
     static void CriarArquivo()
     {
-        var caminhoNovoArquivo = "contaExportadas.csv";
-        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
         {
-            var contaComoString = "456, 7895,4785.40 gustavo santos";
+            var contaComoString = "456,78945,4785.40,Gustavo Santos";
             var encoding = Encoding.UTF8;
-
             var bytes = encoding.GetBytes(contaComoString);
-            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+
+            fluxoDoArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDoArquivo))
+        {
+            escritor.Write("456,65465,456.0,Pedro");
         }
     }
 }
