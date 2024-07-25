70593acbd8b5b330bacb473f566a4f3516e6ec63
fabriciofelix <felixfabricio115@gmail.com>
2024-07-25 13:38:06 -0300

    binaryWriter e binaryReader

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,19 +4,30 @@ partial class Program
 {
     static void Main(string[] args)
     {
-            var caminhoNovoArquivo = "TestaEscrita.txt";
-
-            using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-            using (var escritor = new StreamWriter(fluxoDoArquivo))
-            {
-                escritor.WriteLine(true);
-                escritor.WriteLine(false);
-                escritor.WriteLine(454545454545);
-                
-            }
-
-            Console.WriteLine("aplicação finalizada");
+       
+                EscritaBinaria();
+                LeituraBinaria();
         
-            Console.ReadLine();
+                Console.ReadLine();
+            
+        
     }
 }
+
+
+
+
+// var caminhoNovoArquivo = "TestaEscrita.txt";
+//
+// using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+// using (var escritor = new StreamWriter(fluxoDoArquivo))
+// {
+//     escritor.WriteLine(true);
+//     escritor.WriteLine(false);
+//     escritor.WriteLine(454545454545);
+//     
+// }
+//
+// Console.WriteLine("aplicação finalizada");
+//
+// Console.ReadLine();
