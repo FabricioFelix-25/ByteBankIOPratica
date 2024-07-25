f5c194e1cc527e02d49e8948a7e505039ca1bcd1
fabriciofelix <felixfabricio115@gmail.com>
2024-07-25 13:16:21 -0300

    escrita binaria

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,7 +4,19 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        TestarEscrita();   
-        Console.ReadLine();
+            var caminhoNovoArquivo = "TestaEscrita.txt";
+
+            using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDoArquivo))
+            {
+                escritor.WriteLine(true);
+                escritor.WriteLine(false);
+                escritor.WriteLine(454545454545);
+                
+            }
+
+            Console.WriteLine("aplicação finalizada");
+        
+            Console.ReadLine();
     }
 }
