86d48bb6886983b82c0ecbc58736c692d33b26b0
fabriciofelix <felixfabricio115@gmail.com>
2024-07-29 10:06:19 -0300

    auxiliares de classe file

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,32 +1,24 @@
-﻿
-using System.Text;
-
-partial class Program
+﻿static void Main(string [] args)
 {
-    static void Main(string[] args)
+    //Console.WriteLine("Digite seu nome:");
+    //var nome = Console.ReadLine();
+
+    var linhas = File.ReadAllLines("contas.txt");
+    Console.WriteLine(linhas.Length);
+
+    /*
+    foreach (var linha in linhas)
     {
-        UsarStreamDeEntrada();
-
-        Console.WriteLine("Aplicação Finalizada ...");
-
-        Console.ReadLine();
+        Console.WriteLine(linha);
     }
+    */
+
+    //var bytesArquivo = File.ReadAllBytes("contas.txt");
+    //Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+    //File.WriteAllText("escrevendoComClasseFile.txt", "Testando File.WriteAllText");
+
+    Console.WriteLine("Aplicação Finalizada ...");
+
+    Console.ReadLine();
 }
-
-
-
-
-// var caminhoNovoArquivo = "TestaEscrita.txt";
-//
-// using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-// using (var escritor = new StreamWriter(fluxoDoArquivo))
-// {
-//     escritor.WriteLine(true);
-//     escritor.WriteLine(false);
-//     escritor.WriteLine(454545454545);
-//     
-// }
-//
-// Console.WriteLine("aplicação finalizada");
-//
-// Console.ReadLine();
